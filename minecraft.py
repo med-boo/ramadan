47a78fbaeb105f5feaba3a84bc9f2bc7572bd58c
med-boo <molay2003mohamed@gmail.com>
2025-04-08 23:23:47 -0400

    books and magic

--- minecraft.py
+++ minecraft.py
@@ -1,8 +1,8 @@
 class Weapons ():
-    def __init__(self, durability, made_of):
+    def __init__(self, durability, made_of, breakable = True ):
         self.durability = durability
         self.madeof = made_of
-    
+        self.breakable = breakable
 
 class Plants ():
     def __init__(self):
@@ -10,8 +10,8 @@ class Plants ():
 
 
 class Dagger(Weapons):
-    def __init__(self, durability, made_of, shape, color = "wooden", is_enchanted = False):
-        super().__init__(durability, made_of)
+    def __init__(self, durability, made_of, breakable, shape, color = "wooden", is_enchanted = False):
+        super().__init__(durability, made_of,breakable)
         self.shape = shape
         self.color = color
         self.isenchanted = is_enchanted
@@ -20,7 +20,9 @@ class Dagger(Weapons):
             if self.durability > 0:
                 if thing.texture == "ARBORESCENTE":
                     print(thing.gives(self.isenchanted))
-                    self.durability -= 20
+                    if self.breakable:
+                        self.durability -= 20
+                    
                     return "you have cut the plant and the durability of your weapon has decreased."
                 else:
                     return("Your weapon Can't cut this plant.")
@@ -33,8 +35,8 @@ class Dagger(Weapons):
                 
 
 class Axe(Weapons):
-    def __init__(self, durability, made_of,shape, color = "silver", is_enchanted = False):
-        super().__init__(durability, made_of)
+    def __init__(self, durability, breakable, made_of,shape, color = "silver", is_enchanted = False):
+        super().__init__(durability, made_of,breakable)
         self.shape = shape
         self.color = color
         self.isenchanted = is_enchanted
@@ -43,7 +45,9 @@ class Axe(Weapons):
             if self.durability > 0:
                 if thing.texture == "ARBORESCENTE" or thing.texture == "TREE":
                     print(thing.gives(self.isenchanted))
-                    self.durability -= 5
+                    if self.breakable:
+
+                        self.durability -= 5
 
                     return "you have cut the plant and the durability of your weapon has decreased."
                 else:
@@ -55,9 +59,10 @@ class Axe(Weapons):
             return("You can't break a non-plant thing with your weapon")
 
 class Sword(Weapons):
-     def __init__(self, durability, made_of,shape, color = "silver", is_enchanted = False):
-        super().__init__(durability, made_of)
+     def __init__(self, durability,made_of, shape, sharpeness,breakable, color = "silver", is_enchanted = False, ):
+        super().__init__(durability, made_of,breakable)
         self.shape = shape
+        self.sharpness = sharpeness
         self.color = color
         self.isenchanted = is_enchanted
      def cut_things(self, thing):
@@ -65,7 +70,8 @@ class Sword(Weapons):
             if self.durability > 0:
                 if thing.texture == "ARBORESCENTE" or thing.texture == "TREE" or thing.texture == "PERENNIAL":
                     print(thing.gives(self.isenchanted))
-                    self.durability -= 10
+                    if self.breakable:
+                        self.durability -= 10
                     return "you have cut the plant and the durability of your weapon has decreased."
                 else:
                     return("Your weapon Can't cut this plant.")
@@ -114,15 +120,88 @@ class Sugar_can(Plants):
         else:
             return("You got 1 fruit.")
 
-axe = Axe(50, "stone", "round", is_enchanted= True)
-straw = Strawberry("red", "big")
-arsalan = Dagger(60, "Iron", "Aigu", "silver", True)
-chajara = Pinetree("brown", 3)
-for i in range(11):
-    if arsalan.durability == 0:
-        break
-    
-    print(arsalan.cut_things(straw))
-    print(arsalan.durability)
+# axe = Axe(50, "stone", "round", is_enchanted= True)
+# straw = Strawberry("red", "big")
+# arsalan = Dagger(60, "Iron", "Aigue", "silver", True)
+# chajara = Pinetree("brown", 3)
+
+
+# if arsalan.durability == 0:    
+#     print(arsalan.cut_things(straw))
+#     print(arsalan.durability)
 
 #print(straw.gives(True))
+# TODO enchant the weapons to encrease there durability.
+class Magic_books():
+    def __init__(self, magic_type):
+        self.magicstype = magic_type
+    def enchant(self, sla7):
+        pass
+
+class Unbreaking(Magic_books):
+    def __init__(self, level, magic_type = "power"):
+        super().__init__(magic_type)
+        self. level = level
+
+    def enchant(self, sla7):
+        if isinstance(sla7, Weapons):
+            sla7.durability += 10 * self.level
+            return (f"Your weapon has been enchanted with unbreaking {self.level} .")
+        else:
+            return "These books can only be applied to weapons!"
+
+# book = Unbreaking(2)
+# print(arsalan.durability)
+# print(book.enchant(arsalan))
+# print(arsalan.durability)
+
+class Sharpeness(Magic_books):
+    def __init__(self, level, magic_type = "sharp"):
+        super().__init__(magic_type)
+        self. level = level
+    def enchant(self, sla7):
+        if isinstance(sla7, Sword):
+            sla7.sharpness += 20 * self.level
+            return (f"You have applied sharpness{self.level} to your sword.")
+        else:
+            return "This book can only be applied to a sword."
+
+# book = Sharpeness(1)
+sayf = Sword(100, "diamond", "long", 100,True, "silver", is_enchanted= True)
+# print(sayf.sharpness)
+# print(book.enchant(sayf))
+# print(sayf.sharpness)
+
+# print(sayf.durability)
+# sayf.durability += 30
+# print(sayf.durability)
+# print(sayf.color)
+# sayf.color = "red"
+# print(sayf.color)
+
+
+class Hard(Magic_books):
+    def __init__(self, magic_type = "Hardness"):
+        super().__init__(magic_type)
+    
+    def enchant(self, sla7):
+        if isinstance(sla7, Weapons):
+            sla7.breakable = False
+            return "Your weapon will no longer break"
+        else:
+            return "This book can only be applied to weapons"
+        
+book = Hard()
+chjra = "aziz"
+print(sayf.breakable)
+print(book.enchant(sayf))
+print(sayf.breakable)
+
+        
+            
+        
+
+
+         
+
+        
